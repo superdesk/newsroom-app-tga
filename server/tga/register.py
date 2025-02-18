80abeb259a847d2c6c66f41ba4955b56a7884259
MarkLark86 <mark.pittaway@gmail.com>
2025-02-18 15:05:38 +1100

    [TGA-114] Remove 'Privacy collection statement' (#59)
    * [TGA-114] Remove 'Privacy collection statement'
    
    * fix docker compose

--- server/tga/register.py
+++ server/tga/register.py
@@ -11,9 +11,6 @@ blueprint = Blueprint('register', __name__)
 class RegisterEOIForm(SignupForm):
     company = StringField(lazy_gettext('Organisation / Institute'), validators=[DataRequired()])
     occupation = StringField(lazy_gettext('Position held / Title'))
-    consent_login = BooleanField(lazy_gettext('I consent to'), validators=[])
-    consent_alerts = BooleanField(lazy_gettext('I consent to'), validators=[])
-    consent_reporting = BooleanField(lazy_gettext('I consent to'), validators=[])
     consent_terms = BooleanField(lazy_gettext('I agree to'), validators=[])
 
 
